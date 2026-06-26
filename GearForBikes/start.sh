2c38c21a5a12ec0adf292a87173fe16c4dffc956
Киселев Артур <KiselArt_87@students.mgke.loc>
2026-06-26 12:30:04 +0300

    4

--- GearForBikes/start.sh
+++ GearForBikes/start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-cd GearForBikes\GearForBikes
+
+# НЕ НУЖЕН cd, так как manage.py уже в /app/
+# Если start.sh в корне с manage.py, то cd не нужен
+
+python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-gunicorn GearForBikes.wsgi
+gunicorn GearForBikes.wsgi:application --bind 0.0.0.0:8080
--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd GearForBikes\GearForBikes
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
-gunicorn GearForBikes.wsgi:application --bind 0.0.0.0:8080
